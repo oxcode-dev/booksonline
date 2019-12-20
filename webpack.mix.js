9134a6f86cd2caa755f59f9cd1544ec37ad10e73
= <samuel@tedbree.com>
2019-12-20 18:31:36 +0100

    Rename Folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ mix.js('src/app.js', 'scripts/js')
     .sass('src/app.scss', 'scripts/css')
     .options({
         extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
-        processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+        processCssUrls: false, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
         purifyCss: false, // Remove unused CSS selectors.
         uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
         postCss: [], // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
